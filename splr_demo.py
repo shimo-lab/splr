ec73ae7f179b30ba408708b04bf692e68f98b892
rom1p <23080067+RomainPe@users.noreply.github.com>
2018-09-13 21:06:27 +0900

    update demo

--- splr_demo.py
+++ splr_demo.py
@@ -26,11 +26,7 @@ splr_svd_duration = end_time - start_time
 # scipy SVD for dense matrices
 x_centered_dense = x_centered.toarray()
 
-start_time = time.time()
 u_dense, d_dense, tv_dense = scipy.linalg.svd(x_centered_dense)
-end_time = time.time()
-
-scipy_svd_duration = end_time - start_time
 
 # approximation error for scipy SVD
 err_dense = np.linalg.norm(x_centered_dense - (u_dense[:, :k] * d_dense[:k]) @ tv_dense[:k, :])
@@ -41,6 +37,5 @@ err_splr = np.linalg.norm(x_centered_dense - (u * d) @ v.T)
 # excess approximation error for splr SVD
 ex_rel_approx_err = max(0, (err_splr - err_dense) / err_dense)
 
-print("splr SVD computed in {:.2f} seconds".format(splr_svd_duration))
-print("scipy SVD computed in {:.2f} seconds".format(scipy_svd_duration))
+print("SVD computed in {:.2f} seconds".format(splr_svd_duration))
 print("excess relative approximation error : {0:.2%}".format(ex_rel_approx_err))
